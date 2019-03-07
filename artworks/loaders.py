137505a6cfd8907b654eba7a6b29a6021f717502
yatin <iamyatin@gmail.com>
2019-03-07 20:56:43 +0530

    Minor additions

--- artworks/loaders.py
+++ artworks/loaders.py
@@ -1,14 +1,22 @@
 from scrapy.loader import ItemLoader, Identity
+from scrapy.loader.processors import TakeFirst, MapCompose
+
+
+def add_base_url(value, loader_context):
+    return loader_context['base_url'] + value
 
 
 class ArtworksLoader(ItemLoader):
     """
     Item loader for ArtworksItem
     """
+    default_output_processor = TakeFirst()
     url_in = Identity()
     artist_in = Identity()
-    title_in = Identity()
+    title_in = TakeFirst()
+    image_in = MapCompose(add_base_url)
     height_in = Identity()
     width_in = Identity()
     description_in = Identity()
     path_in = Identity()
+    path_out = Identity()
--- artworks/spiders/trial.py
+++ artworks/spiders/trial.py
@@ -10,8 +10,8 @@ from artworks.loaders import ArtworksLoader
 class TrialSpider(scrapy.Spider):
     name = 'trial'
     # Start with Browse Page: http://pstrial-a-2018-10-19.toscrape.com/browse/
-    # start_urls = ['http://pstrial-a-2018-10-19.toscrape.com/browse/']  #
-    start_urls = ['http://pstrial-a-2018-10-19.toscrape.com/browse/qingjapanese/annejoseph/donstyle']  #
+    start_urls = ['http://pstrial-a-2018-10-19.toscrape.com/browse/']  #
+    # start_urls = ['http://pstrial-a-2018-10-19.toscrape.com/browse/qingjapanese/annejoseph/donstyle']  #
 
     _css_selectors = {
         "main_heading": "h1::text",
@@ -20,7 +20,7 @@ class TrialSpider(scrapy.Spider):
         # "artworks_item_image_relative_url": "::attr('src')",
         "item_page_artist": "h2::text",
         "item_page_title": "h1::text",
-        "item_page_image": "#body img",
+        "item_page_image": "#body img::attr('src')",
     }
 
     _xpath_selectors = {
@@ -39,8 +39,8 @@ class TrialSpider(scrapy.Spider):
         # Put your logic to process artworks directory
         parent_heading = response.meta.get("parent_heading")
         category_heading = response.css(self._css_selectors.get("main_heading")).extract_first()
-        category_heading = category_heading.split("-")[-1].strip()
-        category_heading = parent_heading + "," + category_heading
+        category_heading = category_heading.split("-")[1].strip()
+        category_heading = parent_heading + "," + category_heading if parent_heading else category_heading
 
         # 1. #subcats all h3.. get their h3
         subcats = response.css(self._css_selectors.get("subcats"))
@@ -54,16 +54,16 @@ class TrialSpider(scrapy.Spider):
         # TODO: 2. Iterate and yield the item page
         item_images = response.css(self._css_selectors.get("artworks_item_images"))
         for item_image in item_images:
-            print(item_image)
             item_page = item_image.xpath(self._xpath_selectors.get("artworks_item_relative_url")).get()
             yield response.follow(item_page, self.parse_item, meta={"category": category_heading})
 
         # TODO: 3. Follow next pages for item list
-        next_link = response.xpath(self._xpath_selectors.get("next_link"))
+        next_link = response.xpath(self._xpath_selectors.get("next_link")).get()
         yield response.follow(next_link, self.parse, meta={"parent_heading": category_heading})
 
     def parse_item(self, response):
-        loader = ArtworksLoader(ArtworksItem(), response=response)
+        base_url = self._get_base_url(response.url)
+        loader = ArtworksLoader(ArtworksItem(), response=response, base_url=base_url)
         loader.add_value("url", response.url)
         loader.add_css("artist", self._css_selectors.get("item_page_artist"))
         loader.add_css("title", self._css_selectors.get("item_page_title"))
