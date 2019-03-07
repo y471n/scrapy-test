4b2a28fff1634ea7c316782db791d0e7237fe278
monk <monk@monks-MacBook-Air.local>
2019-03-07 11:53:35 +0530

    Add Gitignore

--- artworks/spiders/trial.py
+++ artworks/spiders/trial.py
@@ -5,8 +5,17 @@ import scrapy
 
 class TrialSpider(scrapy.Spider):
     name = 'trial'
-    start_urls = []  # put your start urls here
+    # Start with Browse Page: http://pstrial-a-2018-10-19.toscrape.com/browse/
+    start_urls = ['http://pstrial-a-2018-10-19.toscrape.com/browse/']  #
 
     def parse(self, response):
         # Put your logic to process artworks directory
+
+        # 1. #subcats all h3.. get their h3
+
+
+        # yield Next Subcategories
+        # 2. Get Art in this category
+        # 3. Yield Next Page's Art in same category page
+
         pass
